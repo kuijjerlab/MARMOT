34bf650753fae57f638da826240ff3948404229e
rtpop <romanatp@uio.no>
2024-03-05 09:22:17 +0100

    added functions to format surv data

--- R/prepare_data.r
+++ R/prepare_data.r
@@ -1,5 +1,6 @@
 #############################################################################
-## Functions to manipulate data and prepare it for JDR with or without PCA ##
+## Functions to manipulate omics  and clinical data and prepare it for JDR ##
+################# and further analysis with or without PCA ##################
 #############################################################################
 
 #' @title Prepare data for JDR.
@@ -202,4 +203,136 @@ prepare_data <- function(omics, names = NULL, sep = NULL,
   }
 
   return(omic_fil)
+}
+
+#' @title Extract survival information from clinical data.
+#'
+#' @name prepare_surv
+#'
+#' @description Function that extracts survival information from a larger
+#' dataframe of clinical features and formats it for downstream analysis.
+#'
+#' @inbheritParams prepare_data
+#' @param clinical Character string containing path to a file with clinical
+#' information.The expected format of clinical data is with rows as samples
+#' and columns as features.
+#' @param feature_names Named list containing the feature names for
+#' the survival features of interest. Two feature names can be
+#' provided per element. If so, the first feature will be used unless a sample
+#' has NA values for that feature, in which case it will check for values in the
+#' second feature. See examples for details. The list must be named as follows:
+#' list(sample_id = "sample_id", vital_status = "vital_status",
+#' time_to_event = "time_to_event")
+#' @param keep_nas Logical. If samples with NA values in any survival feature
+#' should be kept.
+#'
+#' @return A data frame containing survival information.
+#' @examples
+#' @export
+
+prepare_surv <- function(clinical, feature_names,
+                         sep = "\t", keep_nas = FALSE) {
+  # load data
+  clin <- read.table(clinical, sep = sep, head = TRUE)
+
+  # sanity checks
+  # check the features are labelled correctly
+  if (names(feature_names != c("sample_id", "vital_status", "time_to_event"))) {
+    stop("List element names unknown. Please make sure names(feature_names) are
+    as detailed in the documentation and are spelled correctly.")
+  }
+
+  # check if provided feature names exist in clinical data
+  names_exist <- sapply(feature_names, function(x) all(x %in% colnames(clin)))
+  stopifnot(names_exist, message = "Feature names could not be found. Please 
+  make sure feature names exist in clinical data and are spelled correctly.")
+
+  # grab specified features
+  temp <- clin[, unlist(feature_names)]
+
+  # merge columns that were provided for the same feature
+  temp <- lapply()
+
+  feat <- which(sapply(feature_names, function(x) length(x) > 1))
+  if (length(feat > 0)) {
+    temp <- lapply()
+    temp <- ifelse(is.na(temp$days_to_death) & !is.na(temp$days_to_last_followup),
+                                 temp$days_to_last_followup,
+                                 temp$days_to_death)
+  }
+
+
+  # making sure the relevant columns are all named the same
+  # not elegant, but you work with what you got
+  if (TCGA) {
+    clin <- clin %>%
+      rename(
+        sampleID = colnames(clin)[grep("submitter_id.samples", colnames(clin))],
+        days_to_death = colnames(clin)[grep("days_to_death.diagnoses", colnames(clin))],
+        vital_status = colnames(clin)[grep("vital_status.diagnoses", colnames(clin))],
+        days_to_last_followup = colnames(clin)[grep("days_to_last_follow_up.diagnoses", colnames(clin))],
+        days_to_last_followup = colnames(clin)[grep("days_to_lastfollowup", colnames(clin))]
+      )
+
+    #get the relevant columns
+    temp <- data.frame(cbind(sampleID = clin$sampleID, 
+                             vital_status = clin$vital_status, 
+                             days_to_death = clin$days_to_death, 
+                             days_to_last_followup = clin$days_to_last_followup, 
+                             days_to_event = NA))
+
+    #merge days_to_death & days_to_last_followup
+    temp$days_to_death <- as.numeric(temp$days_to_death)
+    temp$days_to_last_followup <- as.numeric(temp$days_to_last_followup)
+    temp$days_to_event <- ifelse(is.na(temp$days_to_death) & !is.na(temp$days_to_last_followup),
+                                 temp$days_to_last_followup,
+                                 temp$days_to_death)
+
+    #change sample names so they match with those in data
+    temp$sampleID <- gsub("-", "\\.", temp$sampleID)
+
+    #replace vital status with logical
+    temp$vital_status <- gsub("DECEASED", TRUE, temp$vital_status, ignore.case = TRUE)
+    temp$vital_status <- gsub("ALIVE", FALSE, temp$vital_status, ignore.case = TRUE)
+    temp$vital_status <- gsub("dead", TRUE, temp$vital_status, ignore.case = TRUE)
+    temp$vital_status <- gsub("LIVING", FALSE, temp$vital_status, ignore.case = TRUE)
+
+    surv <- data.frame(sampleID = temp$sampleID,
+                       vital_status = temp$vital_status,
+                       days_to_event = temp$days_to_event)
+  }else {
+    surv <- data.frame(sampleID = clin$sampleID,
+                       vital_status = clin$vital_status,
+                       days_to_event = clin$time_to_event)
+  }
+
+  surv$days_to_event <- as.numeric(surv$days_to_event)
+  surv$vital_status <- as.logical(surv$vital_status)
+
+  #remove any remaining NAs
+  surv <- surv[!is.na(surv$days_to_event), ]
+  surv <- surv[!is.na(surv$vital_status), ]
+
+  return(surv)
+}
+
+#' @title Merge multiple columns of surv data frame
+#'
+#' @name .merge_feat
+#'
+#' @description Merges the columns of a data frame if more than one feature
+#' is provided for a single feature.
+#'
+#' @inheritParams prepare_surv
+#' @param cols Column names
+#' @param surv Data frame of extracted survival features.
+#'
+#' @return A data frame with merged columns.
+
+.merge_surv <- function(cols, surv) {
+  if (length(cols) > 1) {
+    col <- names(cols)
+    surv[, col] <- ifelse(is.na(surv[, cols[1]]) & !is.na(surv[, cols[2]]),
+                          surv[, cols[2]], surv[, cols[1]])
+  }
 }
