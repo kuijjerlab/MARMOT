8ced5be255d2c53d3f76817df612420a4a134f12
rtpop <romanatp@uio.no>
2024-03-14 15:45:36 +0100

    added tests

--- tests/testthat/test-prepare_data.R
+++ tests/testthat/test-prepare_data.R
@@ -1,8 +1,20 @@
 context("Data manipulation")
 library(JDRnet)
 
+test_that("incorrect file format throws error", {
+  test_files <- c("test_file.fastq")
+  expect_error(.load_data(test_files), 
+    "Invalid file format. Please make sure you provide a text or .RData file.")
+})
+
 test_that("list of correct length is made", {
   test_files <- c("test_file1.Rda", "test_file2.tsv", "test_file3.txt")
-  expect_equal(class(.create_omics_list(test_files)), "list")
+  expect_equal(class(.create_omics_list(test_files, sep = )), "list")
   expect_equal(length(.create_omics_list(test_files)), length(test_files))
+})
+
+test_that("non-overlapping samples throws an error", {
+  test_files <- c("test_file2.tsv", "test_file4.txt")
+  expect_error(.create_omics_list(test_files),
+    "No common samples between the omics. Please ensure omics share at least some samples.")
 })
