38cc987c18a8043ba553f9f2bf7da4a136a3dcf6
rtpop <romanatp@uio.no>
2024-03-15 12:27:25 +0100

    finished functionality for .fct_cutpoint

--- R/differential_analysis.r
+++ R/differential_analysis.r
@@ -10,34 +10,42 @@
 #' defines the groups based on given JDR factors associated with a given
 #' clinical feature.
 #'
+#' @inheritParams surv_association
 #' @param omic A matrix of omics data. Columns should be samples and features
 #' should be rows.
+#' @param clin Optional. A dataframe with clinical features for which to correct
+#' if using limma.
 #' @param factor A dataframe with the factor based on which to determine groups.
-#' @param clin A dataframe of clinical data.
-#' @param feat Character string with the clinical feature with which the factor
-#' of interest is associated. Must be a column name in \code{clin}.
 #' @param covariates Optional. A character vector with names of covariates for
 #' which to correct. Only applies if limma is used.
 #' @param limma Whether limma should be used for the differential analysis.
 #' If FALSE, a wilcoxon signed rank test will be used instead. Default is TRUE.
-#' @param survival Logical. Whether the feature of interest is survival. This
-#' will determine how the groups are divided.
 #' @param minprop Numeric between c(0,1), indicating the minimum proportion of
-#' samples per group. Only used for survival data.
+#' samples per group.
 #'
 #' @export
 
-differential_analysis <- function(omic, factor, clin, feat, covariates = NULL,
-                                  limma = TRUE, survival = TRUE,
+differential_analysis <- function(omic, factor, surv, clin = NULL,
+                                  covariates = NULL, limma = TRUE,
                                   minprop = 0.1) {
   # sanity checks
-  # check that feature name exists in clin data frame
-  .check_names(feat, colnames(clin), err_msg = "feature name exists in the clinical data") # nolint
-
-  # check that covariates exist
   if (!is.null(covariates)) {
-    .check_names(covariates, colnames(clin), err_msg = "covariate names exist in the clinical data") # nolint
+    # check that clinical data is provided if covariates are provided
+    if (is.null(clin)) {
+      stop("Please provide clinical data for covariate correction.")
+    }
+    # check that covariates exist
+    .check_names(covariates, colnames(clin),
+                 err_msg = "covariate names exist in the clinical data")
+
+    # check that limma is selected as the method
+    if (!limma) {
+      stop("Limma must be used for covariate correction.")
+    }
   }
+
+  # define groups
+  df <- .fct_cutpoint(factor = factor, surv)
 
 
 }
@@ -48,47 +56,40 @@ differential_analysis <- function(omic, factor, clin, feat, covariates = NULL,
 #'
 #' @inheritParams differential_analysis
 #' @param factor Factor based on which to split the cohort. Expects a matrix.
-#' @param surv Data frame containing the survival data
 #' @returns Data frame containing the information about the two survival groups.
 
-.fct_cutpoint <- function(factor, surv, minprop = 0.1, survival = TRUE) {
+.fct_cutpoint <- function(factor, surv, minprop = 0.1) {
 
-  # only take samples for which there is survival info
-  samples <- surv$sample_id
+  # only take samples for which there is clin info
+  samples <- surv[, 1]
   samples <- intersect(samples, rownames(factor))
   factor <- factor[samples, ]
-  surv <- surv[which(surv$sample_id %in% samples), ]
+  surv <- surv[which(surv[, 1] %in% samples), ]
 
   # cut the data
   time <- surv$time_to_event
   event <- surv$vital_status
-  df <- data.frame(sample = samples, time = time, event = event, Z = factor)
-  cut <- surv_cutpoint(df, variables = "Z", minprop = minprop)
-  df$FactorValue <- surv_categorize(cut)$Z
+  df <- data.frame(sample = samples, time = time, event = event,
+                   factor = factor)
+  cut <- survminer::surv_cutpoint(df, variables = "factor", minprop = minprop)
+  df$FactorValue <- survminer::surv_categorize(cut)$Z
   df$FactorCluster <- df$Z > cut$cutpoint$cutpoint
-  
-  # determine which is the good survival group and which is the poor survival group
-  df.split <- split(df, df$FactorCluster)
-  if(max(df.split[[1]]$time) > max(df.split[[2]]$time)){
-    df.split[[1]]$group <- "long"
-    df.split[[2]]$group <- "short"
-  }else if(max(df.split[[1]]$time) < max(df.split[[2]]$time)){
-    df.split[[1]]$group <- "short"
-    df.split[[2]]$group <- "long"
+
+  # determine which is the good survival group and which
+  # is the poor survival group
+  # not sure if this is the best way to do it
+  df_split <- split(df, df$FactorCluster)
+  if (max(df_split[[1]]$time) > max(df_split[[2]]$time)) {
+    df_split[[1]]$group <- "long"
+    df_split[[2]]$group <- "short"
+  } else if (max(df_split[[1]]$time) < max(df_split[[2]]$time)) {
+    df_split[[1]]$group <- "short"
+    df_split[[2]]$group <- "long"
   }
-  
-  # determine which group has the high factor values and which has low factor values
-  # if(max(df.split[[1]]$Z) < min(df.split[[2]]$Z)){
-  #   df.split[[1]]$FactorValue <- "low"
-  #   df.split[[2]]$FactorValue <- "high"
-  # }else if(min(df.split[[1]]$Z) > max(df.split[[2]]$Z)){
-  #   df.split[[1]]$FactorValue <- "high"
-  #   df.split[[2]]$FactorValue <- "low"
-  # }
-  
-  df2 <- rbind(df.split[[1]], df.split[[2]])
-  df2 <- df2[order(df2$group),]
-  df2$group <- factor(df2$group, levels = c("long", "short"))
+
+  df2 <- rbind(df_split[[1]], df_split[[2]])
+  df2 <- df2[order(df2$group), ]
+  df2$group <- factor(df2$group)
 
   return(df2)
 }
