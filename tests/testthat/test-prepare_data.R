eb25c6a0a39bb049bc6117f888f116a34d95baa0
rtpop <romanatp@uio.no>
2024-04-07 13:55:59 +0200

    added test for non-numeric data

--- tests/testthat/test-prepare_data.R
+++ tests/testthat/test-prepare_data.R
@@ -3,13 +3,13 @@ library(JDRnet)
 test_that("incorrect file format throws error", {
   test_files <- c("test_file.fastq")
   expect_error(suppressWarnings(.load_data(test_files)),
-    "Invalid file format. Please make sure you provide a supported file format.")
+    "Invalid file format: test_file.fastq. Please make sure you provide a supported file format.")
 })
 
 test_that("list of correct length is made", {
   test_files <- c("test_file1.Rda", "test_file2.tsv", "test_file3.txt")
   expect_equal(class(suppressWarnings(.create_omics_list(test_files, overlap_samples = FALSE))), "list")
-  expect_equal(length(suppressWarnings(.create_omics_list(test_files, overlap_samples = FALSE))), 4)
+  expect_equal(length(suppressWarnings(.create_omics_list(test_files, overlap_samples = FALSE))), 3)
 })
 
 test_that("no overlapping samples throws an error", {
@@ -20,5 +20,12 @@ test_that("no overlapping samples throws an error", {
 
 test_that("too few samples gives a warning", {
   test_files <- c("test_file2.tsv", "test_file5.txt")
-  expect_warning(.create_omics_list((test_files)))
+  expect_warning(.create_omics_list(test_files),
+  "Fewer than 15 samples common to all omics. This may cause unpredictable results.")
+})
+
+test_that("non-numeric data throws an error", {
+  test_files <- c("test_file6.txt")
+  expect_error(suppressWarnings(.create_omics_list(test_files)),
+    "Data is not numeric. Please make sure all your omics are numeric.")
 })
