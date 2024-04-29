f1d7d8eaa1f444a39a259814e07e001508ad628f
rtpop <romanatp@uio.no>
2024-04-29 15:03:55 +0200

    initial commit

---
+++ R/view_contribution.r
@@ -0,0 +1,42 @@
+################################################################################
+## Functions relating to testing the contribution of each view to a MOFA model #
+################################################################################
+
+#' @name fct_corr
+#'
+#' @description Perform pearson and spearman correlation between two sets of JDR
+#' factors. Primarily intended for comparing different JDR models on the same
+#' data.
+#'
+#' @inheritParams view_exclusion
+#' @param set1 A matrix with the first set of factors.
+#' @param set2 A matrix with the second set of factors.
+#'
+#' @returns A list containing the factor correlation matrices.
+#'
+#' @export
+
+fct_corr <- function(set1, set2) {
+  # sanity checks
+  # check that the number of features is the same in the two sets
+  if (nrow(set1) != nrow(set2)) {
+    warning("Differing numbers of features (rows) between sets of factors. Results may be unreliable.") # nolint
+  }
+
+  # perform pairwise correlations of each factor in one set with the other
+  corr_pears <- apply(set1, 2, function(x) {
+    apply(set2, 2, function(y) {
+      cor(x, y, method = "pearson")
+    })
+  })
+
+  corr_spear <- apply(set1, 2, function(x) {
+    apply(set2, 2, function(y) {
+      cor(x, y, method = "spearman")
+    })
+  })
+
+  corr_list <- list(corr_pears, corr_spear)
+
+  return(corr_list)
+}
