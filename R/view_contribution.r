99c1600d58e8e934320b83b4b2f785cf91912030
rtpop <romanatp@uio.no>
2024-04-29 15:29:52 +0200

    added plotting function

--- R/view_contribution.r
+++ R/view_contribution.r
@@ -23,7 +23,7 @@ fct_corr <- function(set1, set2) {
     warning("Differing numbers of features (rows) between sets of factors. Results may be unreliable.") # nolint
   }
 
-  # perform pairwise correlations of each factor in one set with the other
+  # perform pairwise correlations of each factor in one set with the others
   corr_pears <- apply(set1, 2, function(x) {
     apply(set2, 2, function(y) {
       cor(x, y, method = "pearson")
@@ -39,4 +39,20 @@ fct_corr <- function(set1, set2) {
   corr_list <- list(corr_pears, corr_spear)
 
   return(corr_list)
+}
+
+#' @name plot_fct_corr
+#'
+#' @description Plots heatmaps of factor correlations.
+#'
+#' @param corr_list A data frame with the correlation values. Expects output
+#' of \code{\link{fct_corr}}.
+#' @param title Character string indicating a title for the plot.
+#'
+#' @returns A list of two ggplots. One for pearson and one for spearman.
+#'
+#' @export
+
+plot_fct_corr <- function(corr_list, title) {
+  
 }
