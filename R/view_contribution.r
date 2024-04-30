de3687afc16fcff6d1bd6257af9b6ce84484ca00
rtpop <romanatp@uio.no>
2024-04-30 08:58:19 +0200

    adjusting plot dimensions

--- R/view_contribution.r
+++ R/view_contribution.r
@@ -99,11 +99,11 @@ p <- ggplot(pears, aes(x = Var2, y = Var1, fill = value, label = round(value, 2)
   theme_bw()
 
   if (grid) {
-   q <- cowplot::ggdraw(p) +
-        cowplot::draw_plot_label(label = "Plot 1", size = 15),
-        cowplot::ggdraw(s) +
-        draw_plot_label(label = "Plot 2", size = 15),
-  nrow = 1
+    q <- cowplot::plot_grid(cowplot::ggdraw(p) +
+                              cowplot::draw_plot_label(size = 15),
+                            cowplot::ggdraw(s) +
+                              cowplot::draw_plot_label(size = 15),
+                            nrow = 1)
   } else {
     q <- list(pearson = p, spearman = s)
     names(q) <- c("pearson", "spearman")
