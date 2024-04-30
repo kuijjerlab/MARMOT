3fc5afbdbf92ef3b5fca1ed61e3ca640e67b4acc
rtpop <romanatp@uio.no>
2024-04-30 08:31:36 +0200

    added plot + better labelling for fct_corr

--- R/view_contribution.r
+++ R/view_contribution.r
@@ -11,16 +11,23 @@
 #' @inheritParams view_exclusion
 #' @param set1 A matrix with the first set of factors.
 #' @param set2 A matrix with the second set of factors.
+#' @param labels Option character vector with labels for the two sets of factors.
 #'
 #' @returns A list containing the factor correlation matrices.
 #'
 #' @export
 
-fct_corr <- function(set1, set2) {
+fct_corr <- function(set1, set2, labels = NULL) {
   # sanity checks
   # check that the number of features is the same in the two sets
   if (nrow(set1) != nrow(set2)) {
-    warning("Differing numbers of features (rows) between sets of factors. Results may be unreliable.") # nolint
+    warning("Differing numbers of samples (rows) between sets of factors. Results may be unreliable.") # nolint
+  }
+
+  # add labels
+  if (!is.null(labels)) {
+    colnames(set1) <- paste(colnames(set1), labels[1], sep = "_")
+    colnames(set2) <- paste(colnames(set2), labels[2], sep = "_")
   }
 
   # perform pairwise correlations of each factor in one set with the others
@@ -37,6 +44,7 @@ fct_corr <- function(set1, set2) {
   })
 
   corr_list <- list(corr_pears, corr_spear)
+  names(corr_list) <- c("pearson", "spearman")
 
   return(corr_list)
 }
@@ -45,14 +53,37 @@ fct_corr <- function(set1, set2) {
 #'
 #' @description Plots heatmaps of factor correlations.
 #'
-#' @param corr_list A data frame with the correlation values. Expects output
+#' @inheritParams plot_data_dim
+#' @param corr_list A list with the correlation values. Expects output
 #' of \code{\link{fct_corr}}.
 #' @param title Character string indicating a title for the plot.
+#' @param ... Any other ggplot parameters.
 #'
 #' @returns A list of two ggplots. One for pearson and one for spearman.
 #'
 #' @export
 
-plot_fct_corr <- function(corr_list, title) {
+plot_fct_corr <- function(corr_list, title, colours = NULL, ...) {
+  # set colours
+  if (is.null(colours)) {
+    col <- RColorBrewer::brewer.pal(name = "Dark2", n = 8)
+    col <- col[c(3, 5)]
+  } else {
+    if (length(colours) != 1) {
+      stop(paste0(length(colours), " colours were specified, when 2 were expected. ",
+      "Please make sure you specify the correct number of colours."))
+    }
+    col <- colours
+  }
+
+# Convert matrix to a data frame
+pears <- reshape2::melt(corr_list[["pearson"]])
+spear <- reshape2::melt(corr_list[["spearman"]])
+
+# Plot heatmap
+p <- ggplot(pears, aes(x = Var2, y = Var1, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient(low = col[1], high = col[2]) +
+  labs(x = "Column", y = "Row", title = "Heatmap")
   
 }
