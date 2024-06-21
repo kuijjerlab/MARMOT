afc29446962d08b5dd232dbf4d343974375f1f0c
rtpop <romanatab@hotmail.com>
2024-06-21 14:13:32 +0200

    changed how this works and finished functionality

--- R/pca_to_feat_weights.r
+++ R/pca_to_feat_weights.r
@@ -2,96 +2,27 @@
 ## Functions to map factor weights of principle components back to features ##
 ##############################################################################
 
-#' @title Map weights to features.
+#' @name map_wts
 #'
-#' @name map_weights_to_features
+#' @description Function to map back to original feature weights when using
+#' This function multiplies PCA loadings with JDR fctor weights to give a
+#' matrix of the original features (e.g. genes) mapped to the JDR factors.
 #'
-#' @description This function maps factor weights of princiople components
-#' back to individual features (e.g. genes). Performing PCA on omics data prior
-#' to JDR can have several advantages. However, one of the strength of JDR
-#' approaches is being able to identify which individual features contribute
-#' most to the factorisation. In order achieve this when performing PCA first,
-#' we must multiply the matrix of PCA loadings to the matrix of factor weights.
-#' This only works with JDR methods that are linear. For details, check [cite my paper] # nolint
+#' @param fct_weights A matrix of JDR factor weights for an omic.
+#' @param pca_weights A pcaMethods object for the same omic.
 #'
-#' @inheritParams run_jdr
-#' @param fct_list A list of factorissations. Expects output of
-#' \code{\link{run_jdr}}, or one of the individual functions.
-#' @param pca_weights A pcaMethods output corresponding to the data
-#' imputted for JDR. See \code{\link{prepare_data}}.
-#' @param omics Names of the omics for which this should be done.
-#' If NULL, it will be done on all omics
-#'
-#' @return
-#'
-#' @examples
-#'
+#' @returns A matrix of features mapped to factors.
 #' @export
-#'
 
-map_weights_to_features <- function(jdr_methods = c("MOFA", "JIVE", "RGCCA", "MCIA"),
-                                    fct_list, pca_weights,
-                                    omics = NULL) {
-  # get MOFA weights
-  if (is.element("MOFA", jdr_methods)) {
-    mofa_wts <- .map_mofa_wts(fct_list$MOFA, pca_weights, omics)
-  }
+map_wts <- function(fct_weights, pca_weights) {
+  # get pca loadings
+  loadings <- pca_weights@loadings
 
-}
+  #selecting only the PCs used in the mofa analysis
+  loadings <- loadings[, seq_len(nrow(fct_weights))]
 
-#' @name .map_mofa_wts
-#'
-#' @description Internal function that extracts factor weights from a trained
-#' MOFA object and multiplies them with PCA loadings.
-#'
-#' @inheritParams map_weights_to_features
-#' @param mofa_object A MOFA object containing a trained MOFA model.
-#'
-#' @return
-#' #maybe just import specific functions here
-#' @import MOFA2
+  # matrix multiplication between the PCA loadings and the factor weights
+  feat_wts <- loadings %*% fct_weights
 
-.map_mofa_wts <- function(mofa_object, pca_weights, omics) {
-  # get weights
-  weights <- get_weights(mofa_object)
-
-  feat_wts <- list()
-
-  if (is.null(omics)) {
-    omics <- names(weights)
-  }
-
-  #substituting into the MOFA object
-  metadata <- data.frame()
-  dimensions <- c()
-
-  for (j in omics) {
-    loadings <- pca_weights[[j]]@loadings
-    #selecting only the PCs used in the mofa analysis
-    loadings <- loadings[,1:nrow(weights[[j]])]
-    # matrix multiplication between the PCA loadings and the factor weights
-    mult <- loadings %*% weights[[j]]
-    feat_wts[[j]] <- mult
-
-
-    #change the expectations (i.e. actual weight values)
-    mofa_object@expectations$W[[j]] <- feat_wts[[j]]
-
-    #get new feature metadata
-    view_metadata <- data.frame(feature = rownames(feat_wts[[j]]), view = rep(j, nrow(feat_wts[[j]])))
-    metadata <- rbind(metadata, view_metadata)
-    
-    #get new dimensions
-    dimensions <- c(dimensions, nrow(feat_wts[[j]]))
-    
-    
-    #replace feature metadata
-    mofa_object@features_metadata <- metadata
-    
-    #replace dimensions
-    mofa_object@dimensions$D <- dimensions
-  }
-  
-  names(mofa_object@dimensions$D) <- omics
-  return(mofa_object)
+  return(feat_wts)
 }
